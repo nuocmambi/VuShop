1311bec446b84548070c479ae996476a1bd34a83
nuocmambi <nuocmambi@gmail.com>
2016-08-31 22:30:10 +0200

    forste push

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var Order = require('../model/Order');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('order');
+});
+
+
+router.post('/', function(req, res){
+  console.log(req.body.navn);
+
+  res.render('Confirm');
+
+
 });
 
 module.exports = router;
