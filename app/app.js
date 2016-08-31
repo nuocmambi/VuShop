3a399596100da31f6c4fcedf44c97cdf1c784c59
nuocmambi <nuocmambi@gmail.com>
2016-08-31 14:09:27 +0200

    bestillingsskjema

--- app/app.js
+++ app/app.js
@@ -23,6 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', routes);
 app.use('/order', orders);
 app.use('/about', about);
